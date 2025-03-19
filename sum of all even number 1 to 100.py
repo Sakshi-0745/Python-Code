228dd3f746f2156f0a664f08d5ac607b4462d367
Sakshi-0745 <kalashettisakshi@gmail.com>
2025-03-19 21:23:04 +0530

    Add files via upload

---
+++ sum of all even number 1 to 100.py
@@ -0,0 +1,17 @@
+# n= int(input("Enter a number:"))
+# sum=0
+# for i in range(1,n+1):
+#     if i % 2 ==0:
+#         sum+=i
+# print(sum)
+
+# n= int(input("Enter a number:"))
+# sum=0
+# for i in range(2,101,2):
+#     sum+=i
+#     print(sum)
+
+
+n= int(input("Enter a number:"))
+print(sum(list(range(2,101,2))))
+    
