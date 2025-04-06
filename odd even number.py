0c3e87dc8b6d708f4493dab55e4ae211facb036b
Sakshi Shrishail Kalashetti <kalashettisakshi@gmail.com>
2025-04-06 20:04:55 +0530

    Add files via upload

---
+++ odd even number.py
@@ -0,0 +1,8 @@
+def odd_even(num):
+    if num%2==0:
+        return "even"
+    else:
+        return odd
+num=int(input("enter a number"))
+result = odd_even(num)
+print(f"the number is {result}")
