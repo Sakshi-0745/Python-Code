59ed961f19032acbe664e05a0e6004da267f4f1b
Sakshi Kalashetti <kalashettisakshi@gmail.com>
2025-05-03 22:24:38 +0530

    Add files via upload

---
+++ addition of matrix.py
@@ -0,0 +1,10 @@
+import numpy as np
+a = np.array([1,2,3],[4,5,6])
+b = np.array([7,8,9],[10,11,12])
+print("printing the elements of the first matrix")
+print(a)
+print("printing the elements of the second matrix")
+print(b)
+print("printing the sum of the two matrices")   
+c = a + b
+print(c)
